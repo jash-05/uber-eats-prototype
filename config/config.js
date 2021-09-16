c81ca077730701d67fb53567b8261a1d7d9f7fba
jash-05 <jash.sheth@gmail.com>
2021-09-16 12:37:48 -0700

    added connection to database in db.js, added port in config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb"
+    HOST: "uber-eats-clone.c7demyr6eccb.us-west-1.rds.amazonaws.com",
+    PORT: "3306",
+    USER: "admin",
+    PASSWORD: "jashCC05",
+    DB: "uber_eats_db"
   };

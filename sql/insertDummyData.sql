6c28d598ff2d1c752109dc6d7fce8e5f9c0ec03b
jash-05 <jash.sheth@gmail.com>
2021-09-17 01:05:16 -0700

    modified SQL queries used for db

---
+++ sql/insertDummyData.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers( first_name, last_name, email_id, pass, phone_number, dob, city) VALUES 
+	("Max","Verstappen","verstappenmax7@gmail.com","max123","4084121122","06121982","San Francisco"),
+    ("Sebastian","Vettel","sebvettel@gmail.com","seb123","4081231953","09131992","San Jose"),
+    ("Lando","Norris","lando66@gmail.com","lando123","6692015234","03021999","San Jose"),
+    ("Carlos","Sainz","carlos.sainz@gmail.com","carlos123","4084321122","12271995","Palo Alto"),
+    ("Lewis","Hamilton","lewis7xchampion@gmail.com","lewis123","6695567210","07171984","San Francisco");

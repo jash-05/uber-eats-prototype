2dad1ec2dcaacdd7c867c9498f5379b0a25f8db0
jash-05 <jash.sheth@gmail.com>
2021-09-19 19:53:13 -0700

    removed backend files and moved to server folder

--- routes/restaurant_address.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const restaurant_addresses = require("../controllers/restaurant_address.controller.js");
-  
-    // Create a new restaurant address
-    app.post("/restaurantAddress", restaurant_addresses.create);
-  
-    // Retrieve all restaurant addresses
-    app.get("/restaurantAddress/:restaurant_ID", restaurant_addresses.findOne);
-  };

0b1785d345e8b79a7995e9999ebdbba277431cf6
jash-05 <jash.sheth@gmail.com>
2021-10-03 01:22:31 -0700

    added code to  fetch dishes and display bootstrap cards

--- client/src/components/restaurant.details.js
+++ client/src/components/restaurant.details.js
@@ -38,6 +38,7 @@ class RestaurantDetails extends Component{
     //Call the Will Mount to set the auth Flag to false
     componentWillMount(){
         this.fetchRestaurantDetails();
+        this.fetchDishes();
     }
     stateChangeHandler(stateName, stateValue){
         console.log(stateName, stateValue)
@@ -70,29 +71,82 @@ class RestaurantDetails extends Component{
             console.error(err);
         }
     }
-    fetchDishes = async (data) => {
+    fetchDishes = async () => {
+        let dishesData = [] 
+        try {
+            const response = await axios.get('http://localhost:3001/dish', {params:{restaurant_ID: this.state.restaurant_ID}})
+            console.log("Status Code : ",response.status);
+            if(response.status === 200){
+                console.log("Successful request");
+                console.log(response.data);
+                for (let i=0;i < response.data.length; i++){
+                    let main_ingredients = response.data[i].main_ingredients;
+                    if (main_ingredients.length > 20) {
+                        main_ingredients = main_ingredients.slice(0,80).concat('...')
+                    }
+                    dishesData.push({
+                        'dish_ID': response.data[i].dish_ID,
+                        'dish_name': response.data[i].dish_name,
+                        'category_ID': response.data[i].category_ID,
+                        'main_ingredients': main_ingredients,
+                        'price': response.data[i].price,
+                        'about': response.data[i].about,
+                        "dish_image": response.data[i].dish_image
+                    });
+                }
+                this.setState({
+                    fetchedDishes: dishesData
+                })
+                console.log('Cookie status: ', cookie.load('cookie'));
+            } else{
+                console.log("Unsuccessful request");
+                console.log(response);
+            }
+        } catch (err) {
+            console.error(err);
+        }
     }
     render(){
-        console.log("Cover image")
-        console.log(this.state.cover_image)
+        console.log("Fetched dishes")
+        console.log(this.state.fetchedDishes)
+        const createCard = card => {
+            return (
+                <Col sm={3} className="m-3  border"  style={{ width: '30rem', height:'10rem'}}>
+                    <Row className="p-2">
+                        <Col xs={9} md={8}>
+                            <Row className="h5 mt-2 mb-4">{card.dish_name}</Row>
+                            <Row className="text-muted">{card.main_ingredients}</Row>
+                        </Col>
+                        <Col xs={3} md={4}>
+                            <Image src={card.dish_image} className="img-fluid" style={{height: '8rem'}} />
+                        </Col>
+                    </Row>
+                  </Col>
+            )
+        }
         return(
             <Container>
-            <Row>
-                <Col></Col>
-                <Col xs={10}>
-                    <Image src={this.state.cover_image}></Image>
-                </Col>
-                <Col></Col>
-            </Row>
-            <Row className="h2">
-                {`${this.state.restaurant_name} (${this.state.short_address})`}
-            </Row>
-            <Row>
-                {`${this.state.about}`}
-            </Row>
-            <Row>
-                {`${this.state.full_address}`}
-            </Row>
+                <Container>
+                    <Row>
+                        <Col xs={10}>
+                            <Image src={this.state.cover_image} style={{ width: '85rem', height: '20rem'}}></Image>
+                        </Col>
+                    </Row>
+                    <Row className="h2">
+                        {`${this.state.restaurant_name} (${this.state.short_address})`}
+                    </Row>
+                    <Row>
+                        {`${this.state.about}`}
+                    </Row>
+                    <Row>
+                        {`${this.state.full_address}`}
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        {this.state.fetchedDishes.map(createCard)}
+                    </Row>
+                </Container>
             </Container>
         )
     }
